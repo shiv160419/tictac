5e06b0132531083867bde098f46d3b871a9f49b9
shiv160419 <129748540+shiv160419@users.noreply.github.com>
2023-04-03 17:12:50 +0530

    Add files via upload

---
+++ precache-manifest.305d85d283c9214f616a5b549573dfad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3abfe1291ea241c5741d97bfe122c39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a044bb1dd22c3765daa2",
+    "url": "./static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "1c1bc14635db366edda7",
+    "url": "./static/css/main.e128dd68.chunk.css"
+  },
+  {
+    "revision": "a044bb1dd22c3765daa2",
+    "url": "./static/js/2.a55b072a.chunk.js"
+  },
+  {
+    "revision": "1c1bc14635db366edda7",
+    "url": "./static/js/main.d8626f5c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
